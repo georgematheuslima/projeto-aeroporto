308062a683c1ca1b21c6edcfc2e8ab3410899bad
George Matheus <geoorge.m@hotmail.comm.com>
2020-08-25 18:22:13 -0300

    diaria atualizada

--- jose/data_analysis.py
+++ jose/data_analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-url = "https://raw.githubusercontent.com/georgematheuslima/projeto-aeroporto/master/jose/diaria.txt?token=AOPHFUQ4XG6Z7KCBZKGP4Y27IV6PG"
-data = pd.read_csv(url, encoding='latin-1')
+url = "https://raw.githubusercontent.com/georgematheuslima/projeto-aeroporto/master/jose/diaria.txt?token=AOPHFUVJAOSRGVYJHTMXADS7IV7LM"
+data = pd.read_csv(url, sep=" ")
 print(data)
